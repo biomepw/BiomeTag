394bfc22a6ead29b560047e4ede248efbeef3d80
Jordyn <jordyn@husk.pro>
2020-10-24 15:17:32 +0800

    fix relocations and use command framework

--- src/main/java/pw/biome/tag/commands/TagCommand.java
+++ src/main/java/pw/biome/tag/commands/TagCommand.java
@@ -1,54 +1,64 @@
 package pw.biome.tag.commands;
 
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Default;
+import co.aikar.commands.annotation.Description;
+import co.aikar.commands.annotation.Subcommand;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import pw.biome.tag.database.DatabaseHelper;
 import pw.biome.tag.object.TagItem;
 import pw.biome.tag.object.TagPlayer;
 
-public class TagCommand implements CommandExecutor {
+@CommandAlias("tag")
+@Description("Tag commands")
+public class TagCommand extends BaseCommand {
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    @Default
+    @Description("Shows the tagged player")
+    public void onTag(CommandSender sender) {
+        DatabaseHelper.getCurrentTaggedPlayer().thenAccept(username -> sender.sendMessage(ChatColor.GOLD +
+                "Current tagged player: " + ChatColor.RED + username));
+    }
 
-        if (args.length == 0) {
-            DatabaseHelper.getCurrentTaggedPlayer().thenAccept(username -> sender.sendMessage(ChatColor.GOLD +
-                    "Current tagged player: " + ChatColor.RED + username));
+    @Subcommand("give")
+    @CommandPermission("tag.admin")
+    @Description("Gives the player a tag item")
+    public void onTagGive(Player player) {
+        TagPlayer tagPlayer = TagPlayer.getFromUUID(player.getUniqueId());
+        DatabaseHelper.removeTaggedPlayerAndAddTo(tagPlayer);
+        String formatted = String.format("%s Giving tag to '%s'!", ChatColor.GREEN, tagPlayer.getUsername());
+        player.sendMessage(formatted);
+    }
+
+    @Subcommand("force")
+    @CommandPermission("tag.admin")
+    @Description("Forces the tagged player to the given target")
+    public void onTagForce(CommandSender sender, Player target) {
+        TagPlayer tagPlayer = TagPlayer.getFromUUID(target.getUniqueId());
+        if (tagPlayer != null) {
+            DatabaseHelper.removeTaggedPlayerAndAddTo(tagPlayer);
+            tagPlayer.setTimesTagged(tagPlayer.getTimesTagged() - 1);
+            String formatted = String.format("%s Forcing '%s' as tagged!", ChatColor.GREEN, tagPlayer.getUsername());
+            sender.sendMessage(formatted);
         }
+    }
 
-        if (args.length > 0) {
-            if (sender instanceof Player && sender.hasPermission("tag.admin")) {
-                Player player = (Player) sender;
+    @Subcommand("item")
+    @CommandPermission("tag.admin")
+    @Description("Gives the requesting player the Tag item")
+    public void onTagItem(Player player) {
+        player.getInventory().addItem(TagItem.getTagItem());
+        player.sendMessage(ChatColor.GREEN + "Here's the tag!");
+    }
 
-                if (args.length == 1) {
-                    if (args[0].equalsIgnoreCase("give")) {
-                        TagPlayer tagPlayer = TagPlayer.getFromUUID(player.getUniqueId());
-                        DatabaseHelper.removeTaggedPlayerAndAddTo(tagPlayer);
-                        String formatted = String.format("%s Giving tag to '%s'!", ChatColor.GREEN, tagPlayer.getUsername());
-                        player.sendMessage(formatted);
-                    } else if (args[0].equalsIgnoreCase("item")) {
-                        player.getInventory().addItem(TagItem.getTagItem());
-                        player.sendMessage(ChatColor.GREEN + "Here's the tag!");
-                    } else if (args[0].equalsIgnoreCase("sync")) {
-                        DatabaseHelper.syncData().thenRun(() -> player.sendMessage(ChatColor.GREEN + "Data has been synchronised"));
-                    }
-                } else if (args.length == 2) {
-                    if (args[0].equalsIgnoreCase("force")) {
-                        TagPlayer tagPlayer = TagPlayer.getFromName(args[1]);
-
-                        if (tagPlayer != null) {
-                            DatabaseHelper.removeTaggedPlayerAndAddTo(tagPlayer);
-                            tagPlayer.setTimesTagged(tagPlayer.getTimesTagged() - 1);
-                            String formatted = String.format("%s Forcing '%s' as tagged!", ChatColor.GREEN, tagPlayer.getUsername());
-                            player.sendMessage(formatted);
-                        }
-                    }
-                }
-            }
-        }
-        return true;
+    @Subcommand("sync")
+    @CommandPermission("tag.admin")
+    @Description("Forces the local cache to be dropped and to trust the DB instead")
+    public void onTagSync(CommandSender sender) {
+        DatabaseHelper.syncData().thenRun(() -> sender.sendMessage(ChatColor.GREEN + "Data has been synchronised"));
     }
 }
